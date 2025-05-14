7af0f3b6fa17a4c0aad60404baa217a55ab8d51e
hUpLhAWmygz7ro <malteestor@gmail.com>
2025-05-14 14:31:05 +0200

    scheduler erstellt fuer spaeter

---
+++ Backend/src/main/java/com/example/FinTech/alpaca/UpdateScheduler.java
@@ -0,0 +1,20 @@
+package com.example.FinTech.alpaca;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+//Scheduler für Später um immer aktuelle Daten zu haben
+
+@Service
+public class UpdateScheduler {
+        private final MarketDataService marketDataService;
+
+    public UpdateScheduler(MarketDataService marketDataService) {
+        this.marketDataService = marketDataService;
+    }
+
+/*     @Scheduled(cron = "0 0 22 * * ?")
+    public void checkForNewData() {
+        marketDataService.getDailyBars();
+    } */
+}
